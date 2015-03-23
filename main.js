54f6f23c065eddf8883c6ec5df3ab3adf2350ff3
Adam White <evilofbanality@gmail.com>
2015-03-23 11:09:08 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,94 +1,348 @@
-console.log("js is working");
 
-var $ = function (id) { return document.getElementById(id); }
-
-//var displayedTile = $("tileTemplate");
-//var guess = $("guess");
-
+var $ = function(id) { return document.getElementById(id) };
 
 var randomNumber = function (min, max) {
 	return Math.floor ( Math.random() * ( max - min + 1 ) + min );
 }
 
-var tileNumber = randomNumber (1,4);
+var tileNumber = 0;
+var answerArray = []
+var countdownTimer;
+var seconds;
+var gridsize = 0;
+var playOrder = [];
+var tileCorrespondent = 0;
+var boardSize;
 
-var answerArray = [false, false, false, false];
+var tileSetup = function () {
+
+	if (document.getElementById("2x2").selected) {
+		document.getElementById("gameBoard2x2").style.display = "block";
+		document.getElementById("gameBoard2x3").style.display = "none";
+		document.getElementById("gameBoard3x3").style.display = "none";
+		gridsize = 4;
+		tileNumber = randomNumber (1,4)
+		answerArray = [false, false, false, false];
+	} else if (document.getElementById("2x3").selected) {
+		document.getElementById("gameBoard2x3").style.display = "block";
+		document.getElementById("gameBoard3x3").style.display = "none";
+		document.getElementById("gameBoard2x2").style.display = "none";
+		gridsize = 6;
+		tileNumber = randomNumber (1,6)
+		answerArray = [false, false, false, false, false, false];
+	} else if (document.getElementById("3x3").selected) {
+		document.getElementById("gameBoard3x3").style.display = "block";
+		document.getElementById("gameBoard2x3").style.display = "none";
+		document.getElementById("gameBoard2x2").style.display = "none";
+		gridsize = 9;
+		tileNumber = randomNumber (1,9)
+		answerArray = [false, false, false, false, false, false, false, false, false];
+	}
+
+	var initialArray = [];
+
+	var orderTile = function (length) {
+
+		for (i = 1; i < length + 1; i++) {
+			initialArray.push(i);
+		}
+		return;
+	}
+
+	orderTile(gridsize);
+
+	if (document.getElementById("sequential").checked) {
+
+		playOrder = initialArray;
+
+		if (gridsize == 4) {
+			initialArray = ("2x2tileNode" + initialArray.join('2x2tileNode')).split(/(?=2x2tileNode)/);
+			for (var i = 0; i < initialArray.length; i++) {
+				document.getElementById(initialArray[i]).src = "images/tile" + (i + 1) + ".png";
+		}
+		} else if (gridsize == 6) {
+			initialArray = ("2x3tileNode" + initialArray.join('2x3tileNode')).split(/(?=2x3tileNode)/);
+			console.log("initial 2x3 array: " + initialArray);
+			for (var i = 0; i < initialArray.length; i++) {
+				document.getElementById(initialArray[i]).src = "images/tile" + (i + 1) + ".png";
+		}
+		} else if (gridsize == 9) {
+			initialArray = ("3x3tileNode" + initialArray.join('3x3tileNode')).split(/(?=3x3tileNode)/);
+			for (var i = 0; i < initialArray.length; i++) {
+				document.getElementById(initialArray[i]).src = "images/tile" + (i + 1) + ".png";
+		}
+		}
+
+	} else {
+
+		var shuffleTile = function (array) {
+		    var i = array.length,
+		        j = 0,
+		        temp;
+
+		    while (i--) {
+		        j = Math.floor(Math.random() * (i+1));
+		        temp = array[i];
+		        array[i] = array[j];
+		        array[j] = temp;
+		    }
+		    return initialArray;
+		}
+
+		shuffleTile(initialArray);
+
+		var untiledShuffled = initialArray;
+
+		console.log("untiled shuffled: " + untiledShuffled);
+
+		// initialArray = ("2x3tileNode" + initialArray.join('2x3tileNode')).split(/(?=2x3tileNode)/);
+			
+		// console.log("initial 2x3 array: " + initialArray);
+
+		if (gridsize == 4) {
+			initialArray = ("2x2tileNode" + initialArray.join('2x2tileNode')).split(/(?=2x2tileNode)/);
+			for (var i = 0; i < initialArray.length; i++) {
+				document.getElementById(initialArray[i]).src = "images/tile" + (i + 1) + ".png";
+		}
+		} else if (gridsize == 6) {
+			initialArray = ("2x3tileNode" + initialArray.join('2x3tileNode')).split(/(?=2x3tileNode)/);
+			console.log("initial 2x3 array: " + initialArray);
+			for (var i = 0; i < initialArray.length; i++) {
+				document.getElementById(initialArray[i]).src = "images/tile" + (i + 1) + ".png";
+		}
+		} else if (gridsize == 9) {
+			initialArray = ("3x3tileNode" + initialArray.join('3x3tileNode')).split(/(?=3x3tileNode)/);
+			for (var i = 0; i < initialArray.length; i++) {
+				document.getElementById(initialArray[i]).src = "images/tile" + (i + 1) + ".png";
+		}
+		}
+
+
+		var temp = [];
+
+		if (gridsize == 4) {
+			for (var i = 1; i < (initialArray.length + 1); i++) {
+				temp.push(document.getElementById("2x2tileNode" + i).src);
+		}
+		} else if (gridsize == 6) {
+			for (var i = 1; i < (initialArray.length + 1); i++) {
+				temp.push(document.getElementById("2x3tileNode" + i).src);
+		}
+		} else if (gridsize == 9) {
+			for (var i = 1; i < (initialArray.length + 1); i++) {
+			 temp.push(document.getElementById("3x3tileNode" + i).src);
+		}
+		}
+		
+		console.log("temp: " + temp);
+		
+		var temp2 = [];
+		
+ 		for (i = 0; i < temp.length; i++) {
+			temp2.push(parseInt(temp[i].slice(-5, -4)));	
+		 } 
+	
+		playOrder = temp2;
+		
+		console.log("play order: " + playOrder);
+
+	}
+
+
+/* 	console.log("play order: " + playOrder); */
+/* 	console.log("removed character: " + x);
+	console.log("char at: " + y); */
+
+	tileSwitch();
+
+	}
+
+var secondPassed = function() {
+	var remainingSeconds = parseFloat(seconds / 10);
+
+    document.getElementById('timer').innerHTML = remainingSeconds;
+
+    if (seconds == 0) {
+        clearInterval(countdownTimer);
+        document.getElementById('timer').innerHTML = "";
+
+        for (i = 0; i < answerArray.length; i++) {
+			if (answerArray[i] == true) {
+				gameWon = true;
+			} else {
+				gameWon = false;
+				break;
+			}
+		} 
+
+		if (gameWon) { 
+			console.log("Winner!");
+        } else {
+        	gameOver();
+        }
+
+    } else {
+        seconds--;
+    }
+}
+
+
 
 var tileSwitch = function () {
 
-	switch (tileNumber) {
-	case 1:
-		document.getElementById("tileTemplate").src = "images/dogImage1.jpg";
-		break;
-	case 2:
-		document.getElementById("tileTemplate").src = "images/dogImage2.jpg";
-		break;
-	case 3:
-		document.getElementById("tileTemplate").src = "images/dogImage3.jpg";
-		break;
-	case 4:
-		document.getElementById("tileTemplate").src = "images/dogImage4.jpg";
-	break;
-	}	
+	if (countdownTimer != null) {
+		clearInterval(countdownTimer);
+	}
+
+	if (tileNumber) {
+		if (gridsize == 4) {
+			document.getElementById("tileTemplate").src = "images/2x2images/game1/" + tileNumber + ".jpg";
+		} else if (gridsize == 6) {
+			document.getElementById("tileTemplate").src = "images/2x3images/game1/" + tileNumber + ".jpg";
+		} else if (gridsize == 9) {
+			document.getElementById("tileTemplate").src = "images/3x3images/game1/" + tileNumber + ".jpg";
+		}
+	}
+
 	document.getElementById("guess").focus();
+
+	if (document.getElementById("slow").checked) {
+		seconds = 90;
+	} else if (document.getElementById("medium").checked) {
+		seconds = 60;
+	} else {
+		seconds = 30;
+	}
+
+	countdownTimer = setInterval('secondPassed()', 100);
+	
 }
 
 var gamePlay = function () {
+
 	var submittedGuess = parseInt(document.getElementById("guess").value);
 
 	if (submittedGuess == "") {
 		document.getElementById("guess").focus();
 	} else {
-		if (isNaN(submittedGuess) || submittedGuess > 4) {
+		if (isNaN(submittedGuess) || submittedGuess > gridsize) {
 		document.getElementById("verify").firstChild.nodeValue = "Please enter a valid number.";
 		document.getElementById("guess").value = "";
 		document.getElementById("guess").focus();
 		return;
 	}
 	}
+
 	document.getElementById("verify").firstChild.nodeValue = "";
-	if (submittedGuess == tileNumber) {
-		answerArray[tileNumber - 1] = true;
-		if (answerArray[0] == true && answerArray[1] == true && 
-				answerArray[2] == true && answerArray[3] == true ) {
-				//alert("You won!");
-				document.getElementById("tile1").src = "images/dogImage1.jpg";
-				document.getElementById("tile2").src = "images/dogImage2.jpg";
-				document.getElementById("tile3").src = "images/dogImage3.jpg";
-				document.getElementById("tile4").src = "images/dogImage4.jpg";
-				document.getElementById("tileTemplate").src = "images/youWon.jpg";
-				$("guess").value = "";
+
+	var gameWon = false;
+
+	console.log("submitted guess: " + submittedGuess);
+	console.log(answerArray);
+	
+	if (submittedGuess != playOrder[tileNumber - 1]) {
+		gameOver();
+	}
+
+	if (submittedGuess == playOrder[tileNumber - 1]) {
+		answerArray[playOrder[tileNumber - 1] - 1] = true;
+
+		for (i = 0; i < answerArray.length; i++) {
+			if (answerArray[i] == true) {
+				gameWon = true;
 			} else {
+				gameWon = false;
+				break;
+			}
+		}
+
+		if (gameWon) {		
+			var tileArray = [];
+
+			for ( var i = 1; i <= answerArray.length; i++) {
+				tileArray.push("tileNode" + i);
+			}
+
+			for (var i = 0; i < tileArray.length; i++) {
+				if (gridsize == 4) {
+
+					document.getElementsByClassName(tileArray[i]).src = "images/2x2images/game1/" + (i + 1) + ".jpg";
+				} 
+			}
+
 			
-			console.log(answerArray);
+			if (gridsize == 4) {
+					document.getElementById("2x2tileNode1").src = "images/2x2images/game1/" + 1 + ".jpg";
+					document.getElementById("2x2tileNode2").src = "images/2x2images/game1/" + 2 + ".jpg";
+					document.getElementById("2x2tileNode3").src = "images/2x2images/game1/" + 3 + ".jpg";
+					document.getElementById("2x2tileNode4").src = "images/2x2images/game1/" + 4 + ".jpg";
+			} else if (gridsize == 6) {
+					document.getElementById("2x3tileNode1").src = "images/2x3images/game1/" + 1 + ".jpg";
+					document.getElementById("2x3tileNode2").src = "images/2x3images/game1/" + 2 + ".jpg";
+					document.getElementById("2x3tileNode3").src = "images/2x3images/game1/" + 3 + ".jpg";
+					document.getElementById("2x3tileNode4").src = "images/2x3images/game1/" + 4 + ".jpg";
+					document.getElementById("2x3tileNode5").src = "images/2x3images/game1/" + 5 + ".jpg";
+					document.getElementById("2x3tileNode6").src = "images/2x3images/game1/" + 6 + ".jpg";
+			} else if (gridsize == 9) {
+					document.getElementById("3x3tileNode1").src = "images/3x3images/game1/" + 1 + ".jpg";
+					document.getElementById("3x3tileNode2").src = "images/3x3images/game1/" + 2 + ".jpg";
+					document.getElementById("3x3tileNode3").src = "images/3x3images/game1/" + 3 + ".jpg";
+					document.getElementById("3x3tileNode4").src = "images/3x3images/game1/" + 4 + ".jpg";
+					document.getElementById("3x3tileNode5").src = "images/3x3images/game1/" + 5 + ".jpg";
+					document.getElementById("3x3tileNode6").src = "images/3x3images/game1/" + 6 + ".jpg";
+					document.getElementById("3x3tileNode7").src = "images/3x3images/game1/" + 7 + ".jpg";
+					document.getElementById("3x3tileNode8").src = "images/3x3images/game1/" + 8 + ".jpg";
+					document.getElementById("3x3tileNode9").src = "images/3x3images/game1/" + 9 + ".jpg";
+			}
+			
+
+			document.getElementById("tileTemplate").src = "images/youWon.png";
+			document.getElementById("letsBegin").disabled = true;
+			document.getElementById("guess").value = "";
+			document.getElementById("guess").blur();
+			seconds = 0;
+
+		} else {
 
 			do { 
-				tileNumber = randomNumber(1,4);
-			} while ( answerArray[tileNumber - 1] == true ) 
+				tileNumber = randomNumber(1,gridsize);
+			} while ( answerArray[playOrder[tileNumber - 1] - 1] == true ) 
 
-			console.log(tileNumber);
 
-			$("guess").value = "";
+			console.log("answer array: " + answerArray);
+			// console.log("tile number: " + tileNumber);
+
+			document.getElementById("guess").value = "";
 			document.getElementById("guess").focus();
 			tileSwitch();
-			return this;
 		}
 	}
+}
+
+var gameOver = function () {
+	document.getElementById("tileTemplate").src = "images/gameOver.png";
+	document.getElementById("guess").value = "";	
+	document.getElementById("verify").firstChild.nodeValue = "";	
+	document.getElementById("guess").blur();	
+	document.getElementById("letsBegin").disabled = true;	
+	seconds = 0;
+}
+
+var initializeGame = function () {
+	document.getElementById("gameBoard3x3").style.display = "none";
+	document.getElementById("gameBoard2x3").style.display = "none";
+	document.getElementById("gameBoard2x2").style.display = "none";
+	document.getElementById("tileTemplate").src = "images/tileBlank.png";
+	document.getElementById("letsBegin").disabled = false;
+	document.getElementById("guess").blur();
+	document.getElementById("verify").firstChild.nodeValue = "";
+	clearInterval(countdownTimer);
+	gameWon = false;
 	}
 
-	var initializeGame = function () {
-		document.getElementById("tile1").src = "images/tile1.jpg";
-		document.getElementById("tile2").src = "images/tile2.jpg";
-		document.getElementById("tile3").src = "images/tile3.jpg";
-		document.getElementById("tile4").src = "images/tile4.jpg";
-		document.getElementById("guess").focus();
-		answerArray = [false, false, false, false];
-		tileSwitch();
-		gamePlay();
-	}
 
-window.onload = function (){
-	$("letsBegin").onclick = tileSwitch;
-	$("submitGuess").onclick = gamePlay;
-	$("playAgain").onclick = initializeGame;
+window.onload = function () {
+	$("letsBegin").onclick = tileSetup;
+	$("guess").onkeyup = gamePlay;
+	$("resetGame").onclick = initializeGame;
 }
